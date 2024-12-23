ed0ca321ffa8ca41eb889e98f3fc06aec32bf26c
dessie <lynxes.desmond@gmail.com>
2024-12-23 12:10:49 +0000

    extra comments, renamed Cell to Tile

--- board.py
+++ board.py
@@ -3,8 +3,8 @@ from PyQt6.QtWidgets import QFrame, QGridLayout,QPushButton,QSizePolicy
 from PyQt6.QtCore import Qt, QTimer, pyqtSignal, QPoint,QSize
 from PyQt6.QtGui import QPainter, QColor, QBrush, QPixmap,QIcon
 
-class Cell(QPushButton):
-    '''represents a cell on the board, upon which a piece can be placed'''
+class Tile(QPushButton):
+    '''represents a Tile on the board, upon which a piece can be placed'''
 
     def __init__(self, text=None, row=None,col=None, parent=None):
         super().__init__(text, parent)
@@ -13,15 +13,20 @@ class Cell(QPushButton):
         self.board=parent
         self.gridUrl = './assets/grid/Grid Normal.png'
 
+        # assign a grid texture to each Tile depending on its position in the grid
+
+        #left edge
         if self.col == 0:
             if self.row == 0:
                 self.gridUrl = './assets/grid/Grid Top Left Corner.png'
             else:
                  self.gridUrl = './assets/grid/Grid Left Edge.png'
         
+        #top edge
         elif self.row == 0:
             self.gridUrl = './assets/grid/Grid Top Edge.png'
         
+        #right edge
         if self.col == self.board.boardWidth-1:
             if self.row == self.board.boardHeight-1:
                 self.gridUrl = './assets/grid/Grid Bottom Right Corner.png'
@@ -30,25 +35,21 @@ class Cell(QPushButton):
             else:
                 self.gridUrl = './assets/grid/Grid Right Edge.png'
 
+        #bottom edge
         elif self.row == self.board.boardHeight-1:
             if self.col == 0:
                 self.gridUrl = './assets/grid/Grid Bottom Left Corner.png'
             else:
                 self.gridUrl = './assets/grid/Grid Bottom Edge.png'
-
-        
-        
-
-
-        
-        
+    
+        # stylesheet
         self.style = """
         QPushButton {
             border: none;  /* remove border */
             background-color: #eec58a; 
 
             /* grid texture */
-            background-image: url(%s);  /* Path to the texture */
+            background-image: url(%s);  /* path to the texture */
             background-repeat: no-repeat; 
             background-position: center; 
         }
@@ -62,7 +63,7 @@ class Cell(QPushButton):
         self.setStyleSheet(self.style)
 
 class Board(QFrame): 
-    '''the game board, which consists of a grid of Cell objects'''
+    '''the game board, which consists of a grid of Tile objects'''
 
     # SIGNALS
     updateTimerSignal = pyqtSignal(int)  # signal sent when the timer is updated
@@ -76,15 +77,15 @@ class Board(QFrame):
     timerSpeed = 1000  # the timer updates every 1 second
     counter = 10  # the number the counter will count down from
 
-    # CELL AND PIECE PARAMETERS
-    pieceSizeFactor=0.9 # the ratio of the size of the piece to the cell
+    # Tile AND PIECE PARAMETERS
+    pieceSizeFactor=0.9 # the ratio of the size of the piece to the Tile
     
 
     def __init__(self, parent):
         super().__init__(parent)
 
-        #calculate the initial cell size 
-        self.cell_size = self.getCellSize()
+        #calculate the initial Tile size 
+        self.tile_size = self.getTileSize()
 
         self.timer = QTimer(self)  # create a timer for the game
         #self.timer.timeout.connect(self.timerEvent)  # connect timeout signal to timerEvent method
@@ -92,7 +93,7 @@ class Board(QFrame):
         self.start()  # start the game which will start the timer
 
         # initialise the board's state as a 2D array with width equal to boardWidth and height equal to boardHeight
-        # each cell's value will be 0 at the beginning
+        # each Tile's value will be 0 at the beginning
         self.boardState = [[0 for col in range(self.boardWidth) ] for row in range(self.boardHeight)]
         self.printBoardState() 
 
@@ -104,25 +105,35 @@ class Board(QFrame):
         self.whitePiece = QPixmap("./assets/white_piece.png")
         
 
-        # initialise a grid and buttons for each cell, as a physical representation of the board state
+        #initialise a grid layout as a physical representation of the board state
         self.grid = QGridLayout()
         self.grid.setSpacing(0)
-        self.cells = []
+        
+        #a 2D array containing all the tiles on the board
+        self.tiles = []
+
         for row in range(self.boardHeight):
-            cell_row=[]
+            tile_row=[]
             for col in range(self.boardWidth):
-                #initialise Cell object
-                cell_button = Cell(row=row,col=col,parent=self)
-                #connect the cell to a the placePiece function whenever its clicked
-                cell_button.clicked.connect(lambda _, r=row, c=col: self.placePiece(r, c))
-                self.grid.addWidget(cell_button,row,col)
-                cell_row.append(cell_button)
-            self.cells.append(cell_row)
+                #initialise Tile object, passing the current row and column, as well as the board as parameters
+                tile_button = Tile(row=row,col=col,parent=self)
+
+                #connect the Tile to a the placePiece function whenever its clicked
+                tile_button.clicked.connect(lambda _, r=row, c=col: self.placePiece(r, c))
+
+                #add tile to grid
+                self.grid.addWidget(tile_button,row,col)
+
+                #append tile to tile_row
+                tile_row.append(tile_button)
+
+            #append tile_row to tiles
+            self.tiles.append(tile_row)
             
         self.setLayout(self.grid)
 
-    def getCellSize(self):
-        '''gets the cell size. the cell size is calculated using the widget's width and height'''
+    def getTileSize(self):
+        '''gets the Tile size. the Tile size is calculated using the widget's width and height'''
         width = self.width()
         height = self.height()
 
@@ -131,15 +142,15 @@ class Board(QFrame):
 
     def resizeEvent(self, event):
         '''called whenever the window is resized'''
-        new_cell_size = self.getCellSize()
+        new_tile_size = self.getTileSize()
 
-        # only change the cell size if the newly calculated size is different
-        if new_cell_size != self.cell_size:
-            self.cell_size = new_cell_size
+        # only change the Tile size if the newly calculated size is different
+        if new_tile_size != self.tile_size:
+            self.tile_size = new_tile_size
             for row in range(self.boardHeight):
                 for col in range(self.boardWidth):
-                    self.cells[row][col].setFixedSize(self.cell_size,self.cell_size)
-                    self.cells[row][col].setIconSize(QSize(int(self.cell_size * self.pieceSizeFactor), int(self.cell_size * self.pieceSizeFactor)))
+                    self.tiles[row][col].setFixedSize(self.tile_size,self.tile_size)
+                    self.tiles[row][col].setIconSize(QSize(int(self.tile_size * self.pieceSizeFactor), int(self.tile_size * self.pieceSizeFactor)))
 
         super().resizeEvent(event) 
 
@@ -155,7 +166,7 @@ class Board(QFrame):
         2 - occupied by black 
         '''
         print("board state:")
-        print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in self.boardState]))
+        print('\n'.join(['\t'.join([str(Tile) for Tile in row]) for row in self.boardState]))
 
     def start(self):
         '''starts game'''
@@ -184,7 +195,7 @@ class Board(QFrame):
     def placePiece(self, row, col):
         '''places a new piece on the board'''
 
-        #check if the cell is empty
+        #check if the Tile is empty
         if self.boardState[row][col]==0:
             self.boardState[row][col]=self.currentPlayer
 
@@ -192,32 +203,25 @@ class Board(QFrame):
         self.togglePlayer()
 
 
-
-
-    def movePiece(self, newX, newY):
-        '''tries to move a piece'''
-        pass  # Implement this method according to your logic
-
-
     def resetGame(self):
-        '''clears pieces from the board'''
-        # TODO write code to reset game
+        '''sets the entire state to 0, therefore erasing all pieces from the board'''
+        self.boardState = [[0 for col in range(self.boardWidth) ] for row in range(self.boardHeight)]
 
     def drawBoard(self):
         '''checks the board's state and draws pieces where there's a 1 (white) or 2 (black)'''
         for row in range(self.boardHeight):
             for col in range(self.boardWidth):
-                cell_state = self.boardState[row][col]
-                cell_button = self.cells[row][col]
+                tile_state = self.boardState[row][col]
+                tile_button = self.tiles[row][col]
 
-                if cell_state==1:
+                if tile_state==1:
                     piece = QIcon(self.whitePiece)
-                elif cell_state==2:
+                elif tile_state==2:
                     piece = QIcon(self.blackPiece)
                 else:
                     piece=QIcon()
 
-                cell_button.setIcon(piece)
+                tile_button.setIcon(piece)
 
         #self.printBoardState()
                 
